3d4c63bc261c0e06ef0e03a937c59dbe392d065f
Daniela Sinclair <dasfluid@gmail.com>
2020-12-08 15:06:00 +0000

    Implementing JWT

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_135124) do
+ActiveRecord::Schema.define(version: 2020_12_08_130739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,17 @@ ActiveRecord::Schema.define(version: 2020_12_07_135124) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "after_survey_content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
