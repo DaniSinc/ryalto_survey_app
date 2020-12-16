79f2cf5c67ca1738636f0bcb07b1e5227872f5ff
Daniela Sinclair <dasfluid@gmail.com>
2020-12-16 09:17:07 +0000

    Changed page layout for surveys and questions, these need to be transferred to web app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount Rapidfire::Engine => "/rapidfire"
   get '/show', to: 'surveys#show'
-
+  root to: 'pages#home'
+  
   resources :users, param: :username
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
