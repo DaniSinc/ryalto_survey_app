2652c8705ed6f66833fb17f84c186df28b68d3c3
Daniela Sinclair <dasfluid@gmail.com>
2020-12-19 10:01:45 +0000

    Added d-none to tabs in user surveys, will store expired surveys in other table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_235849) do
+ActiveRecord::Schema.define(version: 2020_12_18_105232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_235849) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "after_survey_content"
-    t.integer "duration"
+    t.date "duration"
   end
 
   create_table "users", force: :cascade do |t|
